1349fc5080447bfadbd2954fdc325c0b4486681e
UnrealLink <37017725+UnrealLink@users.noreply.github.com>
2020-03-02 09:58:58 +0100

    add moves as action space

--- pacman.py
+++ pacman.py
@@ -1,6 +1,18 @@
 import gym
 from gym import error
 from gym.utils import closer
+from gym import spaces
+
+import numpy as np
+
+class Moves(spaces.Space):
+    moves = ['U', 'D', 'L', 'R']
+
+    def sample(self):
+        return self.moves[np.random.randint(0, len(self.moves))]
+
+    def contains(self, x):
+        return x in self.moves
 
 env_closer = closer.Closer()
 
@@ -132,134 +144,3 @@ class Env(object):
         self.close()
         # propagate exception
         return False
-
-
-class GoalEnv(Env):
-    """A goal-based environment. It functions just as any regular OpenAI Gym environment but it
-    imposes a required structure on the observation_space. More concretely, the observation
-    space is required to contain at least three elements, namely `observation`, `desired_goal`, and
-    `achieved_goal`. Here, `desired_goal` specifies the goal that the agent should attempt to achieve.
-    `achieved_goal` is the goal that it currently achieved instead. `observation` contains the
-    actual observations of the environment as per usual.
-    """
-
-    def reset(self):
-        # Enforce that each GoalEnv uses a Goal-compatible observation space.
-        if not isinstance(self.observation_space, gym.spaces.Dict):
-            raise error.Error('GoalEnv requires an observation space of type gym.spaces.Dict')
-        for key in ['observation', 'achieved_goal', 'desired_goal']:
-            if key not in self.observation_space.spaces:
-                raise error.Error('GoalEnv requires the "{}" key to be part of the observation dictionary.'.format(key))
-
-    def compute_reward(self, achieved_goal, desired_goal, info):
-        """Compute the step reward. This externalizes the reward function and makes
-        it dependent on an a desired goal and the one that was achieved. If you wish to include
-        additional rewards that are independent of the goal, you can include the necessary values
-        to derive it in info and compute it accordingly.
-        Args:
-            achieved_goal (object): the goal that was achieved during execution
-            desired_goal (object): the desired goal that we asked the agent to attempt to achieve
-            info (dict): an info dictionary with additional information
-        Returns:
-            float: The reward that corresponds to the provided achieved goal w.r.t. to the desired
-            goal. Note that the following should always hold true:
-                ob, reward, done, info = env.step()
-                assert reward == env.compute_reward(ob['achieved_goal'], ob['goal'], info)
-        """
-        raise NotImplementedError
-
-
-class Wrapper(Env):
-    r"""Wraps the environment to allow a modular transformation.
-    This class is the base class for all wrappers. The subclass could override
-    some methods to change the behavior of the original environment without touching the
-    original code.
-    .. note::
-        Don't forget to call ``super().__init__(env)`` if the subclass overrides :meth:`__init__`.
-    """
-    def __init__(self, env):
-        self.env = env
-        self.action_space = self.env.action_space
-        self.observation_space = self.env.observation_space
-        self.reward_range = self.env.reward_range
-        self.metadata = self.env.metadata
-
-    def __getattr__(self, name):
-        if name.startswith('_'):
-            raise AttributeError("attempted to get missing private attribute '{}'".format(name))
-        return getattr(self.env, name)
-
-    @property
-    def spec(self):
-        return self.env.spec
-
-    @classmethod
-    def class_name(cls):
-        return cls.__name__
-
-    def step(self, action):
-        return self.env.step(action)
-
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
-
-    def render(self, mode='human', **kwargs):
-        return self.env.render(mode, **kwargs)
-
-    def close(self):
-        return self.env.close()
-
-    def seed(self, seed=None):
-        return self.env.seed(seed)
-
-    def compute_reward(self, achieved_goal, desired_goal, info):
-        return self.env.compute_reward(achieved_goal, desired_goal, info)
-
-    def __str__(self):
-        return '<{}{}>'.format(type(self).__name__, self.env)
-
-    def __repr__(self):
-        return str(self)
-
-    @property
-    def unwrapped(self):
-        return self.env.unwrapped
-
-
-class ObservationWrapper(Wrapper):
-    def reset(self, **kwargs):
-        observation = self.env.reset(**kwargs)
-        return self.observation(observation)
-
-    def step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        return self.observation(observation), reward, done, info
-
-    def observation(self, observation):
-        raise NotImplementedError
-
-
-class RewardWrapper(Wrapper):
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
-
-    def step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        return observation, self.reward(reward), done, info
-
-    def reward(self, reward):
-        raise NotImplementedError
-
-
-class ActionWrapper(Wrapper):
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
-
-    def step(self, action):
-        return self.env.step(self.action(action))
-
-    def action(self, action):
-        raise NotImplementedError
-
-    def reverse_action(self, action):
-        raise NotImplementedError
