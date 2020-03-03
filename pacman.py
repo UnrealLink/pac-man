cd1f8c53d53d7d50a9b6127dd0a4a91ad45b37e5
UnrealLink <37017725+UnrealLink@users.noreply.github.com>
2020-03-03 17:40:04 +0100

    idem for env

--- pacman.py
+++ pacman.py
@@ -36,13 +36,16 @@ class Env(object):
     reward_range = (-float('inf'), float('inf'))
     spec = None
 
-    def __init__(self, board="board.txt", seed=None, gui_display=False):
+    def __init__(self, board="board.txt", player_spawn=None, seed=None, gui_display=False):
         """
-        Create a pacman env from a txt grid
+        Create a pacman env from a txt board
         """
         self.board = board
         self.grid = Grid()
-        self.grid.create(board)
+        if player_spawn:
+            self.grid.create(board, player_spawn=player_spawn)
+        else:
+            self.grid.create(board)
         self.free_tiles = np.argwhere(64 - (self.grid.grid & 64))
         self.base_seed = seed
         self.seed(self.base_seed)
@@ -62,7 +65,8 @@ class Env(object):
     observation_space = None
 
     def step(self, action):
-        """Run one timestep of the environment's dynamics. When end of
+        """
+        Run one timestep of the environment's dynamics. When end of
         episode is reached, you are responsible for calling `reset()`
         to reset this environment's state.
         Accepts an action and returns a tuple (observation, reward, done, info).
@@ -82,7 +86,8 @@ class Env(object):
         return self.grid, reward, ended, {}
 
     def reset(self):
-        """Resets the state of the environment and returns an initial observation.
+        """
+        Resets the state of the environment and returns an initial observation.
         Returns:
             observation (object): the initial observation.
         """
@@ -92,64 +97,29 @@ class Env(object):
         return self.grid
 
     def render(self, mode='human'):
-        """Renders the environment.
-        The set of supported modes varies per environment. (And some
-        environments do not support rendering at all.) By convention,
-        if mode is:
-        - human: render to the current display or terminal and
-          return nothing. Usually for human consumption.
-        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
-          representing RGB values for an x-by-y pixel image, suitable
-          for turning into a video.
-        - ansi: Return a string (str) or StringIO.StringIO containing a
-          terminal-style text representation. The text can include newlines
-          and ANSI escape sequences (e.g. for colors).
-        Note:
-            Make sure that your class's metadata 'render.modes' key includes
-              the list of supported modes. It's recommended to call super()
-              in implementations to use the functionality of this method.
-        Args:
-            mode (str): the mode to render with
-        Example:
-        class MyEnv(Env):
-            metadata = {'render.modes': ['human', 'rgb_array']}
-            def render(self, mode='human'):
-                if mode == 'rgb_array':
-                    return np.array(...) # return RGB frame suitable for video
-                elif mode == 'human':
-                    ... # pop up a window and render
-                else:
-                    super(MyEnv, self).render(mode=mode) # just raise an exception
+        """
+        Renders the environment.
         """
         if self.gui:
             self.gui.render()
 
     def close(self):
-        """Override close in your subclass to perform any necessary cleanup.
-        Environments will automatically close() themselves when
-        garbage collected or when the program exits.
+        """
+        All necessary cleanup
         """
         pass
 
     def seed(self, seed=None):
-        """Sets the seed for this env's random number generator(s).
-        Note:
-            Some environments use multiple pseudorandom number generators.
-            We want to capture all such seeds used in order to ensure that
-            there aren't accidental correlations between multiple generators.
-        Returns:
-            list<bigint>: Returns the list of seeds used in this env's random
-              number generators. The first value in the list should be the
-              "main" seed, or the value which a reproducer should pass to
-              'seed'. Often, the main seed equals the provided 'seed', but
-              this won't be true if seed=None, for example.
+        """
+        Sets the seed for this env's random number generator(s).
         """
         np.random.seed(seed)
         return
 
     @property
     def unwrapped(self):
-        """Completely unwrap this env.
+        """
+        Completely unwrap this env.
         Returns:
             gym.Env: The base non-wrapped gym.Env instance
         """
